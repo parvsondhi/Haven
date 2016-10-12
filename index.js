c57ece553da02e693c73bb8a52f59cec96c9b434
parvsondhi <parv@airbears2-10-142-0-60.airbears2.1918.berkeley.edu>
2016-10-12 01:33:25 -0700

    adding indeed

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/webhook', function (req, res) {
         if (event.message && event.message.text) {
           if (!kittenMessage(event.sender.id, event.message.text))
           {
-            company = request({
+            request({
     url: 'http://api.indeed.com/ads/apisearch?publisher=7366968708885971&format=json&limit=3&v=2',
     // url: 'http://api.indeed.com/ads/apisearch?publisher=7366968708885971&q=data%20science&l=san%20francisco&format=json&limit=3&v=2',
     method: 'GET',
@@ -41,9 +41,9 @@ app.post('/webhook', function (req, res) {
     var data = JSON.parse(body);
     console.log("data hopefully displayed:");
     console.log(data.results[1].company);
-    return data.results[1].company;
+    sendMessage(event.sender.id, {text: "Echo: " + event.message.text + data.results[1].company});;
 });
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text + company});
+            //sendMessage(event.sender.id, {text: "Echo: " + event.message.text + company});
           }
         }
     }
