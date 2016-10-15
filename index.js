477ae61bd33b25b7f465d7ce5b702efac9affada
parvsondhi <parv@airbears2-10-142-1-237.airbears2.1918.berkeley.edu>
2016-10-14 23:14:39 -0700

    adding api ai

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ app.post('/webhook', function (req, res) {
 
 // generic function sending messages
 function sendMessage(recipientId, message) {
+  console.log("the message " + message)
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
