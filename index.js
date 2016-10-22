d0646c282fc8d185dad186b397f2dea4e26c3fe9
parvsondhi <parv@airbears2-10-142-0-181.airbears2.1918.berkeley.edu>
2016-10-21 23:26:23 -0700

    integrated api ai and indeed

--- index.js
+++ index.js
@@ -56,11 +56,13 @@ app.post('/webhook', function (req, res) {
                     // url: 'http://api.indeed.com/ads/apisearch?publisher=7366968708885971&q=data%20science&l=san%20francisco&format=json&limit=3&v=2',
                     method: 'GET',
                     qs: {q: response.result.parameters.role, l:response.result.parameters.geocity},
-                }, function(error, response, body, replytext) {
+                }, function(error, response, body,replytext) {
                     var data = JSON.parse(body);
                     console.log("data hopefully displayed:");
                     console.log(event.message.text)
                     console.log(data.results[1].company);
+                    console.log("is this is the issue")
+                    console.log(replytext);
                     sendMessage(event.sender.id,{text: replytext})
                     //kittenMessage2(event.sender.id, data.results[1].company, data.results[2].company)
                     //sendMessage(event.sender.id, {text: "Echo: " + event.message.text + data.results[1].company});;
