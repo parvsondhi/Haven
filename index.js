71881fe2a3613044fbaf051180c017c163eead00
parvsondhi <parv@airbears2-10-142-0-181.airbears2.1918.berkeley.edu>
2016-10-21 23:04:43 -0700

    integrated api ai and indeed

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post('/webhook', function (req, res) {
                     console.log("data hopefully displayed:");
                     console.log(event.message.text)
                     console.log(data.results[1].company);
-                    sendMessage(event.sender.id,{text: replytext})
+                    //sendMessage(event.sender.id,{text: replytext})
                     kittenMessage2(event.sender.id, data.results[1].company, data.results[2].company)
                     //sendMessage(event.sender.id, {text: "Echo: " + event.message.text + data.results[1].company});;
                 });
