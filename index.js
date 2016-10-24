445dfd267c6a43d382d6f70393ced73e04c08316
parvsondhi <parv@airbears2-10-142-0-59.airbears2.1918.berkeley.edu>
2016-10-24 02:16:28 -0700

    adding postbacks

--- index.js
+++ index.js
@@ -50,14 +50,16 @@ app.post('/webhook', function (req, res) {
               console.log(response.result.parameters.geocity)
               console.log(response.result.parameters.role)
               replytext = response.result.fulfillment.speech
+              rolesend = response.result.parameters.geocity
+              locsend = response.result.parameters.role
               if(response.result.parameters.geocity && response.result.parameters.role) {
                 request({
                     url: 'http://api.indeed.com/ads/apisearch?publisher=7366968708885971&format=json&limit=3&v=2',
                     // url: 'http://api.indeed.com/ads/apisearch?publisher=7366968708885971&q=data%20science&l=san%20francisco&format=json&limit=3&v=2',
                     method: 'GET',
                     replytext: replytext,
-                    rolesend:response.result.parameters.geocity,
-                    locsend: response.result.parameters.role,
+                    rolesend: rolesend,
+                    locsend: locsend,
                     qs: {q: response.result.parameters.role, l:response.result.parameters.geocity},
                 }, function(error, response, body) {
                     var data = JSON.parse(body);
