194fc764f9a70c30e24733ba067af8755150a072
parvsondhi <parv@airbears2-10-142-0-81.airbears2.1918.berkeley.edu>
2016-10-27 05:48:37 -0700

    default lcoation

--- index.js
+++ index.js
@@ -152,17 +152,17 @@ else {
               locsend = response.result.parameters.geocity
               //loc_city = locsend
 
-              if(response.result.parameters.defaultlocation){
-                if(response.result.parameters.geocity){
-                loc_city = response.result.parameters.geocity;}
-                if(response.result.parameters.geostate){
-                  loc_city = response.result.parameters.geostate;
-                }
-                sendMessage(event.sender.id,{text: replytext})
-
-
-              }
-              else if(response.result.parameters.geocity && response.result.parameters.role) {
+              // if(response.result.parameters.defaultlocation){
+              //   if(response.result.parameters.geocity){
+              //   loc_city = response.result.parameters.geocity;}
+              //   if(response.result.parameters.geostate){
+              //     loc_city = response.result.parameters.geostate;
+              //   }
+              //   sendMessage(event.sender.id,{text: replytext})
+              //
+              //
+              // }
+              if(response.result.parameters.geocity && response.result.parameters.role) {
                 request({
                     url: 'http://api.indeed.com/ads/apisearch?publisher=7366968708885971&format=json&limit=20&v=2',
                     // url: 'http://api.indeed.com/ads/apisearch?publisher=7366968708885971&q=data%20science&l=san%20francisco&format=json&limit=3&v=2',
@@ -189,36 +189,36 @@ else {
 
               }
 
-              else if(response.result.parameters.role) {
-                replytext = "I found " + response.result.parameters.role + " jobs for you in " + loc_city + " If you want me to find similar roles in another city, try something like 'Find " + response.result.parameters.role + " jobs in Charleston'";
-                console.log("loc_city#######))))))")
-                console.log(loc_city)
-                locationcity = loc_city
-                request({
-                    url: 'http://api.indeed.com/ads/apisearch?publisher=7366968708885971&format=json&limit=20&v=2',
-                    // url: 'http://api.indeed.com/ads/apisearch?publisher=7366968708885971&q=data%20science&l=san%20francisco&format=json&limit=3&v=2',
-                    method: 'GET',
-                    replytext: replytext,
-                    rolesend: rolesend,
-                    locsend: locationcity,
-                    qs: {q: response.result.parameters.role, l: loc_city},
-                }, function(error, response, body) {
-                    var data = JSON.parse(body);
-                    console.log("data hopefully displayed:");
-                    console.log(event.message.text)
-                    console.log(data.results[1].company);
-                    console.log("is this is the issue")
-                    console.log(replytext);
-                    sendMessage(event.sender.id,{text: replytext})
-                    random_numbers = findrandom();
-                    i = random_numbers[0]
-                    j = random_numbers[1]
-                    k = random_numbers[2]
-                    richMessage(event.sender.id, data.results[i].company, data.results[j].company, data.results[k].company, data.results[i].jobtitle, data.results[j].jobtitle, data.results[k].jobtitle, data.results[i].url, data.results[j].url, data.results[k].url, data.results[i].snippet, data.results[j].snippet, data.results[k].snippet,rolesend, loc_city)
-                    //sendMessage(event.sender.id, {text: "Echo: " + event.message.text + data.results[1].company});;
-                });
-
-              }
+              // else if(response.result.parameters.role) {
+              //   replytext = "I found " + response.result.parameters.role + " jobs for you in " + loc_city + " If you want me to find similar roles in another city, try something like 'Find " + response.result.parameters.role + " jobs in Charleston'";
+              //   console.log("loc_city#######))))))")
+              //   console.log(loc_city)
+              //   locationcity = loc_city
+              //   request({
+              //       url: 'http://api.indeed.com/ads/apisearch?publisher=7366968708885971&format=json&limit=20&v=2',
+              //       // url: 'http://api.indeed.com/ads/apisearch?publisher=7366968708885971&q=data%20science&l=san%20francisco&format=json&limit=3&v=2',
+              //       method: 'GET',
+              //       replytext: replytext,
+              //       rolesend: rolesend,
+              //       locsend: locationcity,
+              //       qs: {q: response.result.parameters.role, l: loc_city},
+              //   }, function(error, response, body) {
+              //       var data = JSON.parse(body);
+              //       console.log("data hopefully displayed:");
+              //       console.log(event.message.text)
+              //       console.log(data.results[1].company);
+              //       console.log("is this is the issue")
+              //       console.log(replytext);
+              //       sendMessage(event.sender.id,{text: replytext})
+              //       random_numbers = findrandom();
+              //       i = random_numbers[0]
+              //       j = random_numbers[1]
+              //       k = random_numbers[2]
+              //       richMessage(event.sender.id, data.results[i].company, data.results[j].company, data.results[k].company, data.results[i].jobtitle, data.results[j].jobtitle, data.results[k].jobtitle, data.results[i].url, data.results[j].url, data.results[k].url, data.results[i].snippet, data.results[j].snippet, data.results[k].snippet,rolesend, loc_city)
+              //       //sendMessage(event.sender.id, {text: "Echo: " + event.message.text + data.results[1].company});;
+              //   });
+              //
+              // }
 
               else if(response.result.parameters.No){
                 sendButtonMessage(event.sender.id, replytext)
