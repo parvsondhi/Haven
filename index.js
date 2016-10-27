edad4b2949784048c83e93677484d5c903b83e65
parvsondhi <parv@airbears2-10-142-0-81.airbears2.1918.berkeley.edu>
2016-10-27 04:49:51 -0700

    default lcoation

--- index.js
+++ index.js
@@ -284,9 +284,9 @@ else {
               // url: 'http://api.indeed.com/ads/apisearch?publisher=7366968708885971&q=data%20science&l=san%20francisco&format=json&limit=3&v=2',
               method: 'GET',
               replytext: replytext,
-              rolesend: roletobesearched,
               locsend: locationtobesearched,
-              qs: {q: roletobesearched, l:locationtobesearched},
+              rolesend: roletobesearched,
+              qs: {q: roletobesearched, l: locationtobesearched},
           }, function(error, response, body) {
               var data = JSON.parse(body);
               console.log("data hopefully displayed:");
